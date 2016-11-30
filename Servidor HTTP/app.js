d8b10c38a459dee8c997bbe62db41a5374298c82
sanierjd <sanierjd@live.com>
2016-11-30 15:03:06 -0500

    Usuarios

--- Servidor HTTP/app.js
+++ Servidor HTTP/app.js
@@ -19,9 +19,14 @@ var usuarios = [
     }
 ]
 
+var contador = 3;
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
-  
+})
+
+/*
+app.post('/TecnologiasWeb', function (req, res) {
   // request req
   // response res
   
@@ -52,6 +57,55 @@ app.get('/', function (req, res) {
     res.json(usuario);    
   
 })
+*/
+
+app.get('/Usuario', function (req, res) {
+
+    res.json(usuarios);
+    
+})
+
+app.get('/Usuario/:idUsuario', function (req, res) {
+    
+    var idActual = req.params.idUsuario;
+    for(var i=0;i<usuarios.length;i++){
+        if(idActual == usuarios[i].id){
+            res.json(usuarios[i]);
+        }
+    }
+    res.send('No existe el Usuario');
+    
+})
+
+app.post('/Usuario', function (req, res) {
+    
+    
+    console.log(req.query.nombre);
+    console.log(req.query.cedula);
+    
+    
+    if(!req.query.nombre){
+        res.send('no envio nombre');
+    }
+    
+    if(!req.query.cedula){
+        res.send('no envio cedula');
+    }
+    
+    var nuevoUsuario={
+        id:contador+1,
+        nombre:req.query.nombre,
+        cedula:req.query.cedula
+    }
+    
+    usuarios.push.
+    
+    
+    
+    res.json(usuarios);
+})
+
+// IMPLEMENTAR PUT Y DELETE
 
 app.listen(5050, function () {
   console.log('Example app listening on port 5050!')
