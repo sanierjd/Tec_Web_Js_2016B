4a1623cb840c723a1541b0c7e51854c559c8ac43
sanierjd <sanierjd@live.com>
2016-12-21 14:09:36 -0500

    Administrador y usuario

---
+++ Servidor HTTP/Mascotas/api/models/Usuario.js
@@ -0,0 +1,26 @@
+/**
+ * Usuario.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+      nombres:{
+          type:'string',
+          minLength:5
+      },
+      apellidos:{
+          type:'string',
+          minLength:5
+      },
+      correo:{
+          type:'string',
+          email:true,
+          defaultsTo:'correo@invalido.com'
+      }
+  }
+};
+
