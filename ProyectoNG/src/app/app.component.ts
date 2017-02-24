6271169542cddb1d2b733bc4cb2ffd884aa37749
sanierjd <sanierjd@live.com>
2017-02-24 09:31:16 -0500

    Caombios

--- ProyectoNG/src/app/app.component.ts
+++ ProyectoNG/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+
+// Identar: Ctrl + alt + L
+export class AppComponent implements OnInit{
+  title:string = "Hola amigos";
+  nombre:string = "";
+  apellido:string = "";
+  constructor(){
+    this.apellido = "Loya";
+    this.nombre = "Santiago";
+    console.log("Inició el constructor")
+  }
+  ngOnInit(){
+    this.apellido = "Nasimba";
+    this.nombre = "Xavier";
+    console.log("On Init")
+  }
+  nombreCompleto():string{
+    return `${this.nombre} ${this.apellido}`
+  }
+  hizoClick(){
+    console.log("Hizo Click")
+  }
+  hizoFocus(){
+    console.log("Hizo Focus")
+  }
 }
